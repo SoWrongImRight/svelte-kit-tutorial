7b2a00043246c080be88f295f0b2350ab31c5807
Russ Carroll <info@rcarroll.dev>
2021-10-07 22:19:08 -0400

    Tested parameter based file names.

---
+++ src/routes/posts/[slug]-[date].svelte
@@ -0,0 +1,1 @@
+<h3>Slug Detail</h3>
--- src/routes/posts/__layout.reset.svelte
+++ src/routes/posts/__layout.reset.svelte
@@ -5,7 +5,7 @@
 		<ui>
 			<li><a href="/posts">Home</a></li>
 			<li><a href="/posts/about">About</a></li>
-			<li><a href="">Three</a></li>
+			<li><a href="/">Home Home</a></li>
 			<li><a href="">Four</a></li>
 		</ui>
 	</aside>
