24b3a93740bb399d3b481333ad743df5ea300dc5
Russ Carroll <info@rcarroll.dev>
2021-10-07 21:46:37 -0400

    Initial commit, added postcss and tailwinds.

---
+++ src/routes/about.svelte
@@ -0,0 +1,11 @@
+<h1>About Us</h1>
+<p>
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure quo non nemo quasi? Ex deserunt a
+	labore? Voluptatum veritatis, dolores eum voluptates et tempore odit quaerat! Animi eveniet
+	asperiores nisi dolor officia beatae nihil dignissimos adipisci aspernatur nesciunt, et eius
+	quidem modi assumenda repellat culpa exercitationem ducimus optio necessitatibus doloribus, eaque
+	repellendus qui. Natus quaerat soluta quisquam delectus ex, tenetur earum, sequi iusto nisi beatae
+	sed corrupti. Laudantium impedit provident consequuntur voluptatibus, atque culpa nam? Tempore id
+	sed accusamus dignissimos distinctio magnam ipsum ad maiores soluta? Facere, inventore neque
+	consectetur pariatur, iure accusamus aperiam ipsa, unde qui minima ullam eius?
+</p>
